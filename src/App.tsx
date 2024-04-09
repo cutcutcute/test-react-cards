003734bc3d363d92b339b157d2232e810d972429
cutcutcute <victor.samsonov1402@mail.ru>
2024-04-09 23:02:55 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "../src/app/styles/styles.scss";
+import { Card } from './widgets/Card';
 
-function App() {
+
+const cardItmes: number[] = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+
+function App(): React.JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="card-area">
+        {cardItmes.map((cardIndex) => (
+          <Card key={cardIndex}/>
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/widgets/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Card = (): React.JSX.Element => {
+    return <div className="card-area__element">
+        <header className="card-area__element-title">
+            Lorem Ipsum is simply dummy text
+        </header>
+
+        <div className="card-area__element-content">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </div>
+    </div>
+}
